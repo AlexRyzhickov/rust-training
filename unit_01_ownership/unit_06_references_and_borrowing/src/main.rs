4cd3dbe5e2eaab6e19c9bc9c2809c17cbf8d09de
AlexRyzhickov <makseljoinb@gmail.com>
2025-01-02 19:07:24 +0300

    unit_07_references_and_borrowing

--- unit_01_ownership/unit_06_references_and_borrowing/src/main.rs
+++ unit_01_ownership/unit_06_references_and_borrowing/src/main.rs
@@ -7,3 +7,15 @@ fn main() {
     println!("{}, {}", r1, r2);
 
 }
+
+// fn main() {
+//     let mut s = String::from("hello");
+//
+//     {
+//         let r1 = &mut s;
+//         println!("{}", r1);
+//     } // r1 goes out of scope here, so we can make a new reference with no problems.
+//
+//     let r2 = &mut s;
+//     println!("{}", r2);
+// }
