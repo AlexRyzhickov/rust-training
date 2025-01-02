6d76b510889e6b9af145409323c98434213d4f32
AlexRyzhickov <makseljoinb@gmail.com>
2025-01-02 19:12:19 +0300

    unit_08_dangling_references

---
+++ unit_01_ownership/unit_08_dangling_references/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let reference_to_nothing = dangle();
+}
+
+fn dangle() -> &String {
+    let s = String::from("hello");
+
+    &s
+}
+
+// fn dangle() -> String {
+//     let s = String::from("hello");
+//
+//     s
+// }
